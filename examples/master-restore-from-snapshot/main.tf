3f07cbd3d2bf28068bc47ef2a5f32e127262d0d6
Vincent Tjianattan <t-vincent.tjianattan@traveloka.com>
2021-09-26 03:29:56 +0700

    Updated the example 'master-restore-from-snapshot'

--- examples/master-restore-from-snapshot/main.tf
+++ examples/master-restore-from-snapshot/main.tf
@@ -2,18 +2,21 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-# Create an IAM Role for RDS Enhanced Monitoring
-data "aws_iam_policy" "rds_enhanced_monitoring" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-}
+# Existing VPC with a DB Subnet Group
 data "aws_vpc" "test_vpc" {
   filter {
     name   = "tag:ProductDomain"
-    values = ["tsi"]
+    values = ["test"]
   }
 }
+
+# Existing IAM Policy for RDS Enhanced Monitoring
+data "aws_iam_policy" "rds_enhanced_monitoring" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+
 module "rds_enhanced_monitoring" {
-  source = "github.com/traveloka/terraform-aws-iam-role.git//modules/service?ref=master"
+  source = "git@github.com:traveloka/terraform-aws-iam-role.git//modules/service?ref=v3.0.0"
 
   role_identifier  = "RDS Enhanced Monitoring"
   role_description = "Provides access to Cloudwatch for RDS Enhanced Monitoring"
@@ -27,6 +30,7 @@ resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
   policy_arn = data.aws_iam_policy.rds_enhanced_monitoring.arn
 }
 
+
 module "rds_postgres" {
   source = "../../"
 
@@ -36,7 +40,7 @@ module "rds_postgres" {
   description    = "Postgres to store abctest data"
 
   instance_class    = "db.t2.small"
-  engine_version    = "9.5.10"
+  engine_version    = "9.6.6"
   allocated_storage = 20
   storage_encrypted = false
 
@@ -54,7 +58,7 @@ module "rds_postgres" {
   db_subnet_group_name = "tvlk-staging-rds-subnet-group"
 
   # Change to valid parameter group name
-  parameter_group_name = "default.postgres9.5"
+  parameter_group_name = "default.postgres9.6"
   maintenance_window   = "Mon:00:00-Mon:03:00"
   backup_window        = "21:00-23:00"
 
